c4d7e0950be1d13f2ecc697c0dce3d213a3237f3
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-06-09 13:10:40 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -7,8 +7,8 @@ from pyrogram.errors import FloodWait
 logging.basicConfig(level=logging.INFO)
 
 # Get API ID and API hash from environment variables
-API_ID = int(environ.get("API_ID", 0))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", 22710783))
+API_HASH = environ.get("API_HASH", "616ea341acfed51f916506c20b8a0a44")
 
 if not API_ID or not API_HASH:
     raise ValueError("API_ID and API_HASH must be set")
