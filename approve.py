639b0e5089043433a6ab301e2e6e161e2355c185
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-06-09 13:16:02 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -9,12 +9,12 @@ logging.basicConfig(level=logging.INFO)
 # Get API ID and API hash from environment variables
 API_ID = int(environ.get("API_ID", 22710783))
 API_HASH = environ.get("API_HASH", "616ea341acfed51f916506c20b8a0a44")
-
+SESSION_NAME = "my_account"
 if not API_ID or not API_HASH:
     raise ValueError("API_ID and API_HASH must be set")
 
 # Create a new Client instance and save the session to a file
-User = Client(name="AcceptUser", api_id=API_ID, api_hash=API_HASH, session_name="my_account")
+User = Client(name="AcceptUser", api_id=API_ID, api_hash=API_HASH, session_name=SESSION_NAME)
 
 @User.on_message(filters.command(["run", "approve"], [".", "/"]))                     
 async def approve(client, message):
