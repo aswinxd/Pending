2729bda4c6121a51252974e0ec3a4f874874630b
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-09-10 10:02:31 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -44,7 +44,7 @@ async def approve_requests(client, chat_id):
 
                 try:
                      await client.approve_chat_join_request(chat_id, request.user.id)
-                 except BadRequest as e:
+                except BadRequest as e:
                      if "400 INPUT_USER_DEACTIVATED" in str(e):
                         logging.warning(f"Cannot approve user {request.user.id}: User is deleted ac")
                         continue
