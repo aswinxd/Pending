c16f335627685341800e81d5d48f269c1f1077e5
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-09-10 10:05:22 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -40,7 +40,7 @@ async def approve_requests(client, chat_id):
                     else:
                         raise e
 
-               #  await asyncio.sleep(1)  
+                    await asyncio.sleep(1)  
 
                 try:
                      await client.approve_chat_join_request(chat_id, request.user.id)
