99b312f8491defcdf92193fa77fa1398738df5aa
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-09-10 09:36:17 +0530

    Update approve.py

--- approve.py
+++ approve.py
@@ -6,47 +6,52 @@ from pyrogram.errors import FloodWait, BadRequest
 
 logging.basicConfig(level=logging.INFO)
 
-
 API_ID = int(environ.get("API_ID", 22710783))
 API_HASH = environ.get("API_HASH", "616ea341acfed51f916506c20b8a0a44")
 SESSION_NAME = "my_account"
 if not API_ID or not API_HASH:
     raise ValueError("API_ID and API_HASH must be set")
-    
+
 User = Client(name="AcceptUser", api_id=API_ID, api_hash=API_HASH)
 
+BATCH_SIZE = 100 
+
 async def approve_requests(client, chat_id):
-    logging.info(f"Starting {chat_id}")
+    logging.info(f"Starting approval in chat {chat_id}")
     
     while True:
         try:
-            await client.approve_all_chat_join_requests(chat_id)
-           # logging.info(f"Approved {chat_id}")
-            await asyncio.sleep(0.5) 
-        except FloodWait as e:
-            return
+            requests = await client.get_chat_join_requests(chat_id, limit=BATCH_SIZE)
             
-            #logging.warning(f"FloodWait {e.value} seconds")
-            await asyncio.sleep(e.value)
+            if not requests:
+                logging.info("No more pending join requests.")
+                await client.send_message(chat_id, "All pending join requests have been approved.")
+                break
+            for user in requests:
+                await client.approve_chat_join_request(chat_id, user.user.id)
+               # logging.info(f"Approved user: {user.user.id}")
+
+            await asyncio.sleep(1) 
+
+        except FloodWait as e:
+            logging.warning(f"FloodWait: Sleeping for {e.value} seconds.")
+            await asyncio.sleep(e.value) 
         except BadRequest as e:
-            return
-           # logging.error(f"BadRequest error")
+            logging.error(f"BadRequest error: {str(e)}")
             if "HIDE_REQUESTER_MISSING" in str(e):
-                return
-               #logging.info("continue")
-                await asyncio.sleep(0.5)  
+               # logging.info("HIDE_REQUESTER_MISSING, stopping.")
+                break
             else:
                 break
         except Exception as e:
-            return
-            #logging.error(f"Unexpected error: {str(e)}")
+            logging.error(f"Unexpected error: {str(e)}")
             break
 
-@User.on_message(filters.command(["run", "approve"], [".", "/"]))                     
+@User.on_message(filters.command(["run", "approve"], [".", "/"]))
 async def approve(client, message):
     chat_id = message.chat.id
     await message.delete()
-    await client.send_message(chat_id, "Approval process started. Approving pending join requests...")
+#    await client.send_message(chat_id, "Approval process started. Approving pending join requests...")
     asyncio.create_task(approve_requests(client, chat_id))
 
 if __name__ == "__main__":
